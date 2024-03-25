ccd8f57c7385ea16e2bb76cd6154b5c27b4006fb
jasonkiptoo <kiptoojason@gmail.com>
2024-03-25 21:54:32 +0300

    new start initialised new rrepo with all the modules workingg tailwind and primevue installed

---
+++ layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+    <header class="bg-black-300">
+        <div class="text flex justify-around  p-2 ">
+
+            <p class="justify-center ">Free shipping on orders above KES 2,000</p>
+            <p class="">help</p>
+        </div>
+
+    </header>
+   <header class="bg-green-400">
+   <ul class="flex container gap-3 p-4 mx-auto h-14 w-full justify-start">
+    <li>Home</li>
+    <li>Products</li>
+    <li>About</li>
+    <li>Contact us</li>
+<li><InputGroup>
+    <InputGroupAddon>
+        <Icon nam=""/>
+        <!-- <i class="pi pi-user"></i> -->
+    </InputGroupAddon>
+    <InputText placeholder="Username" />
+</InputGroup></li>
+   </ul>
+
+   
+</header>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/about/index.vue
@@ -0,0 +1,13 @@
+<template>
+   <NuxtLayout>
+    khk
+   </NuxtLayout>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
